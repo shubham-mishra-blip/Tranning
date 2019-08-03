f9841ea2ef9096c699e43ae2324231f180c2b852
Shubham <rawshubham443@gmail.com>
2019-08-03 10:33:41 +0530

    done

--- GUVI/src/EvenOrOdd.java
+++ GUVI/src/EvenOrOdd.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class EvenOrOdd {
+
+    public static void main(String args[]){
+
+        //Initialise Scanner
+        Scanner lo = new Scanner(System.in);
+
+        //Taking Input
+        System.out.println("Enter any Number");
+        int num = lo.nextInt();
+
+        //CHECKING Even or Odd
+        if (num % 2 == 0){
+            System.out.print("Number is Even");
+        }
+        else
+            System.out.print("Number is Odds");
+    }
 }
